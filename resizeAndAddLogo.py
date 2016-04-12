e1c8dcdd1cc8d9e64669bcb45f091206c07fdc79
Bobby Gatlin <bgatlin@mail.valenciacollege.edu>
2016-04-12 18:10:52 -0400

    attempt 3

--- resizeAndAddLogo.py
+++ resizeAndAddLogo.py
@@ -4,19 +4,25 @@ from PIL import Image
 SQUARE_FIT_SIZE = 300
 LOGO_FILENAME = 'logo.png'
 
-logoIm = Image.open(LOGO_FILENAME)
+logoIm = Image.open( LOGO_FILENAME )
 logoWidth, logoHeight = logoIm.size
 
-os.makedirs("./withLogo", exist_ok=True)
-for filename in os.listdir('.'):
+os.makedirs('withLogo', exist_ok=True)
+#TODO: Loop over all files in the working directory
+for filename in os.listdir('originals'):
     if not (filename.endswith('.png') or filename.endswith('.jpg')) \
-    or filename == LOGO_FILENAME:
-        continue
+       or filename == LOGO_FILENAME:
+        continue # skip non-image files and the logo file itself
+    
+    im = Image.open(filename)
+    width, height = im.size
 
-im = Image.open(filename)
-width, height = im.size
+#TODO: Check if file image needs to be resized
 
-if width > SQUARE_FIT_SIZE and height > SQUARE_FIT_SIZE:
+    if width > SQUARE_FIT_SIZE and height > SQUARE_FIT_SIZE:
+   
+        #TODO: Calculate the new width and height to resize to.
+
         if width > height:
             height = int((SQUARE_FIT_SIZE / width) * height)
             width = SQUARE_FIT_SIZE
@@ -24,11 +30,15 @@ if width > SQUARE_FIT_SIZE and height > SQUARE_FIT_SIZE:
             width = int((SQUARE_FIT_SIZE / height) * width)
             height = SQUARE_FIT_SIZE
 
-print('Resizing %s...' % (filename))
-im = im.resize((width, height))
-
+        # Resize the image.
+            print('Resizing %s...' % (filename))
+            im = im.resize((width, height))
+    
+#TODO: Add the logo.
+       
 print('Adding logo to %s...' % (filename))
 im.paste(logoIm, (width - logoWidth, height - logoHeight), logoIm)
+       
+#TODO: Save changes.
 
-im.save(os.path.join("./withLogo" , filename))
-            
+im.save(os.path.join('withLogo', filename))
